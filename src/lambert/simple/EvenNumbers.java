56577279ebe010c2afb5d2e979a1eba2f4b57068
Shlok Bhatt <shlokbhatt3@gmail.com>
2024-10-20 21:59:24 -0400

    Completed first ACSL problem 1

--- src/lambert/simple/EvenNumbers.java
+++ src/lambert/simple/EvenNumbers.java
@@ -2,6 +2,18 @@ package lambert.simple;
 
 public class EvenNumbers {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int count = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+                count++;
+            }
+        }
+        System.out.println("Total count of even numbers: " + count);
     }
 }
+
+
+/*Problem 1: Print Even Numbers
+
+Write a Java program that prints all even numbers from 1 to 100 using a for loop.*/
